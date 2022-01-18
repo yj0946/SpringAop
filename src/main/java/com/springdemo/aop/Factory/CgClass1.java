ed84f226880a61b83adfcad965e73a8e94ebe776
yj0946 <yj123_wo@163.com>
2022-01-19 01:28:50 +0800

    2022-1-19 1:28 dev SpringAop提交

--- src/main/java/com/springdemo/aop/Factory/CgClass1.java
+++ src/main/java/com/springdemo/aop/Factory/CgClass1.java
@@ -7,8 +7,5 @@ public class CgClass1 {
 
     public void method2(String test) {
         System.out.println("com.xm.ggn.test.proxy.CgClass1.method2");
-        int k = 0;
-        k++;
-        k--;
     }
 }
